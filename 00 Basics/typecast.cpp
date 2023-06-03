a2a08ca79dd91f2e2cfefd79911e50e9a5d84269
OsirisMedici <contactabhish7@gmail.com>
2023-06-03 23:44:35 +0530

    #shubharam

---
+++ 00 Basics/typecast.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    // int a = '4';
+    // cout << a << endl;
+
+    int b = 57;
+    cout << b << endl;
+}
--- Java/Basics/non-primitive.java
+++
@@ -1,9 +0,0 @@
-public class Non_Primitive {
-
-    public static void main(String[] args) {
-           
-    String myString = "Shiny,😉 !";
-    System.out.println(myString);
-
-    }
-}
