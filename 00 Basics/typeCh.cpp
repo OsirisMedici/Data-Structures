3de585ba2f6254f8ddcf7bf5a14aa69299f86acf
OsirisMedici <contactabhish7@gmail.com>
2023-06-09 23:58:49 +0530

    build: :fire: Loop Number

---
+++ 00 Basics/typeCh.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    char ch = 98;
+    cout << ch << endl;
+}
---
+++ 01 Loops and If statements/number.cpp
@@ -0,0 +1,11 @@
+// This code prints the numbers from 1 to 10.
+for (int i = 1; i <= 10; i++)
+{
+    std::cout << i << " ";
+}
+
+// This code prints the even numbers from 2 to 100.
+for (int i = 2; i <= 100; i += 2)
+{
+    std::cout << i << " ";
+}
