49a894f82a101cf9c2d2ea4b6961fc0741d670e9
OsirisMedici <contactabhish7@gmail.com>
2023-06-13 11:05:54 +0530

    Basic Patterns

---
+++ 02 Patterns/pattern.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << "Realy";
+    int n;
+    cout << "Enter a NNumber baby: ";
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+    {
+        cout << i << endl;
+    }
+    
+   
+    cin >> n;
+}
